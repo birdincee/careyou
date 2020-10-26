8bc997268b22573cd231f2c5949257dc805e2552
birdl <incee.jan@bncc.ac.th>
2020-10-26 16:39:44 +0700

    Initial commit

---
+++ lib/utility/utility_ui.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+class Utility {
+  //colors
+  static var lowPink = Color(0xffFF9494);
+  static var lowGreen = Color(0xff43D1A5);
+  static var lowWhite = Color(0xffECFCF6);
+  static var lowBlack = Color(0xff182339);
+}
